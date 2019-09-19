abe6cef556dfa5377e4fd6eb65a41c69e34e0be5
Brandon Menc <brandonmenc@icloud.com>
2019-09-19 14:42:03 -0500

    Fix asdf script load path

--- .zsh/asdf.sh
+++ .zsh/asdf.sh
@@ -1,3 +1,6 @@
-. $HOME/.asdf/asdf.sh
-. $HOME/.asdf/completions/asdf.bash
+ASDF_DIR=$(brew --prefix asdf)
+[ ! -d $ASDF_DIR ] && ASDF_DIR=$HOME/.asdf
+
+. $ASDF_DIR/asdf.sh
+. $ASDF_DIR/completions/asdf.bash
 
