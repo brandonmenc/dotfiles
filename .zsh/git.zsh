18d4488188ec331d3202da8fd2ad243b95f188cb
Brandon Menc <brandonm@spiceworks.com>
2020-06-21 11:05:21 -0500

    Add git fixup alias

--- .zsh/git.zsh
+++ .zsh/git.zsh
@@ -3,4 +3,5 @@ alias gs='git status'
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%C(bold blue)<%an>%Creset' --abbrev-commit"
 
 alias glg='git lg'
+alias gfix='git commit --fixup $(git rev-parse HEAD)'
 
